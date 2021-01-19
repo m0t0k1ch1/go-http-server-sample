86556eec6459c1aafeeffe4685530c415e5ca124
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2021-01-19 23:23:44 +0900

    [pkg] refactoring

--- pkg/handlers/ping.go
+++ pkg/handlers/ping.go
@@ -1,18 +1,14 @@
 package handlers
 
 import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-
 	"github.com/m0t0k1ch1/go-http-server-sample/pkg/common"
 )
 
 // HandlePing is a sample HandlerFunc.
 func HandlePing(env *common.Env, c *common.Context) error {
 	if err := env.RDB.Ping(); err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		return c.InternalServerError(err)
 	}
 
-	return c.String(http.StatusOK, "pong")
+	return c.SuccessWithEmpty()
 }
