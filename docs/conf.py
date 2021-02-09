a895aa423e07f1ddf3435a886c45f1fd52c2bf31
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2021-02-09 23:57:52 +0900

    [docs] update pygments_style: sphinx -> paraiso-dark

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-pygments_style = 'sphinx'
+pygments_style = 'paraiso-dark'
 
 
 # -- Options for HTML output -------------------------------------------------
